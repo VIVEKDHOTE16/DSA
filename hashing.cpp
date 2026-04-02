bf8d40f78fdf92c9f9a54d263d7dbc7a30a0a2ae
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-04-02 23:28:14 +0530

    run: /Users/vivekdhote/Desktop/VIVEK/C++/hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -1,20 +1,22 @@
-# include<iostream>
+#include<iostream>
 using namespace std;
 
 int main(){
-    string s;
-    cin>>s;
-
-    int hash[26]={0};
-    for(int i = 0; i<s.size(); i++){
-        hash[s[i]-'a']++;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n ; i++){
+        cin>>arr[i];
+    }
+    map<int,int>mpp:
+    for(int i=0; i<n;i++){
+        mpp[arr[i]]++;
     }
     int q;
     cin>>q;
     while(q--){
-        char c;
-        cin>>c;
-        cout<<hash[c-'a']<<endl;
+        int num;
+        cin>>num;
+        cout<<mpp[num]<<endl;
     }
-    return 0;
 }
