41f10565c485552dec11b415558cbe3fd09fda13
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-04-08 00:41:28 +0530

    run: /Users/vivekdhote/Desktop/VIVEK/C++/sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -73,8 +73,8 @@ void merge(vector<int> &arr, int low , int mid , int high){
 void mS(vector<int> &arr,int low,int high){
     if(low>=high) return;
     int mid= (low+high)/2;
-    mS=(arr,low,mid);
-    mS=(arr,mid+1,high);
+    mS(arr,low,mid);
+    mS(arr,mid+1,high);
     merge(arr,low,mid,high);
 }
 
