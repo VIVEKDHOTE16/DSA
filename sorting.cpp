d231b2c74f50b41a8fc1cf15116b986e68d24419
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-04-08 01:34:54 +0530

    run: /Users/vivekdhote/Desktop/VIVEK/C++/sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -78,6 +78,30 @@ void mS(vector<int> &arr,int low,int high){
     merge(arr,low,mid,high);
 }
 
+void partion(vector<int> &arr,int low,int high){
+    int pivot=arr[low];
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(arr[i]<=arr[pivot] && i<= high-1){
+            i++;
+        }
+        while(arr[j]>=arr[pivot] && j>= low+1){
+            j++;
+        }
+        if(i<j) swap(arr[i],arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+
+}
+void qs(vector<int> &arr,int low,int high){
+    if(low<high){
+        int pIndex=partion(arr,low,high);
+        qs(arr,low,pIndex-1);
+        qs(arr,pIndex+1,high);
+    }
+}
 
 
 int main() {
@@ -88,7 +112,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    mS(arr,0,n-1); 
+    qs(arr,0,n-1); 
 
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
