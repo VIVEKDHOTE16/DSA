dbad80e139d50c230a199bb8230e22e8a24aa728
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-04-08 00:25:47 +0530

    run: /Users/vivekdhote/Desktop/VIVEK/C++/sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -41,6 +41,36 @@ void insertion_sort( int arr[],int n){
     }
 }
 
+void merge(vector<int> &arr, int low , int mid , int high){
+    vector<int>temp;
+
+    if left=low;
+    right=mid+1;
+
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++
+        }
+
+    }
+    for(int i=low;i<=high;i++){
+        arr[i]=temp[i-low];
+    }
+void mS(vector<int> &arr,int low,int high){
+    if(low>=high) return;
+    int mid= (low+high)/2;
+    mS=(arr,low,mid);
+    mS=(arr,mid+1,high);
+    merge(arr,low,mid,high);
+}
+
+}
+
 int main() {
     int n;
     cin >> n;
@@ -49,7 +79,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    insertion_sort(arr, n); 
+    mS(arr, n); 
 
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
