ef663d0608be4a74007f9a6e1bcbee8fb606a63f
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-04-13 00:29:29 +0530

    run: /Users/vivekdhote/Desktop/VIVEK/C++/sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -159,7 +159,37 @@ int missingNum(vector<int>& arr) {
     return xor1 ^ xor2;
 }
 
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int maxi = 0;
+    int cnt = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] == 1){
+            cnt++;
+            maxi = max(maxi, cnt);   // correct line
+        } else {
+            cnt = 0;
+        }
+    }
+
+    return maxi;
+}
+
 int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+
+    int result = findMaxConsecutiveOnes(nums);
+    cout << "Maximum consecutive 1s: " << result << endl;
+
+    return 0;
+}
+//int main() {
     int n;
     cin >> n;
 
